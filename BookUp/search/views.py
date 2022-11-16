60cf5bee368e7db567aa8e89c902973baf9976b1
Diksha Srivastava <dikshas2911@gmail.com>
2022-11-16 21:29:47 +0530

    api text integrated, but still not dynamic

--- BookUp/search/views.py
+++ BookUp/search/views.py
@@ -2,22 +2,34 @@ from django.shortcuts import render, HttpResponse
 import requests
 
 
-# Create your views here.
 # def index(request):
-#     context = {
-#         'variable' : "Homepage"
-#     }
-#     return render(request, 'bookDetect.html', context)
-    # return HttpResponse("This is a Homepage")
+#     text = "To be or not to be"
+#     # if request.method == 'POST':
+#     #     text = request.POST['text']
+#     url = "https://www.googleapis.com/books/v1/volumes?q={}&key=AIzaSyCVx4jHICEs3Dt8VYlxupTeibnYhBziBkU"
+#     response = requests.get(url.format(text)).json()
+        
+#     book_detail = {
+#                 "book_name" : response['items'][0]['volumeInfo']['title'],
+#                 "author_name" : response['items'][0]['volumeInfo']['authors'],
+#                 "book_description" : response['items'][0]['volumeInfo']['description']
+#             }
+        
+
+#     context = {"book_detail" : book_detail}
+
+#     return render(request, "bookDetect.html", context)
+
+
 
 def index(request):
-    text = "To be or not to be"
-    # if request.method == 'POST':
-    #     text = request.POST['text']
-    url = "https://www.googleapis.com/books/v1/volumes?q={}&key=AIzaSyCVx4jHICEs3Dt8VYlxupTeibnYhBziBkU"
-    response = requests.get(url.format(text)).json()
-        
-    book_detail = {
+    if request.method=='POST':
+        query = request.POST['query']
+        print(query)
+        url = "https://www.googleapis.com/books/v1/volumes?q={}&key=AIzaSyCVx4jHICEs3Dt8VYlxupTeibnYhBziBkU"
+        response = requests.get(url.format(query)).json()
+        # print(response['items'][0]['volumeInfo']['title'])
+        book_detail = {
                 "book_name" : response['items'][0]['volumeInfo']['title'],
                 "author_name" : response['items'][0]['volumeInfo']['authors'],
                 "book_description" : response['items'][0]['volumeInfo']['description']
@@ -25,5 +37,5 @@ def index(request):
         
 
     context = {"book_detail" : book_detail}
+    return render(request, 'BookDetect.html', context)
 
-    return render(request, "bookDetect.html", context)
