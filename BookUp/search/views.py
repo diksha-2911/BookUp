a54186445847c823e9bccc954505a14ced6d2010
Diksha Srivastava <dikshas2911@gmail.com>
2022-11-20 09:41:58 +0530

    voice feature added

--- BookUp/search/views.py
+++ BookUp/search/views.py
@@ -23,21 +23,38 @@ import requests
 
 
 def index(request):
+    query = ""
+    query2= ""
     book_detail = {}
+    context = {}
     if request.method=='POST':
-        query = request.POST['query']
-        print(query)
-        url = "https://www.googleapis.com/books/v1/volumes?q={}&key=AIzaSyCVx4jHICEs3Dt8VYlxupTeibnYhBziBkU"
-        response = requests.get(url.format(query)).json()
-        # print(response['items'][0]['volumeInfo']['title'])
-        book_detail = {
-                "book_name" : response['items'][0]['volumeInfo']['title'],
-                "author_name" : response['items'][0]['volumeInfo']['authors'],
-                "book_description" : response['items'][0]['volumeInfo']['description'],
-                "book_img" : response['items'][0]['volumeInfo']['imageLinks']['smallThumbnail']
-            }
-        
+        query = request.POST['queryspeech']
+        query2 = request.POST['query']
+        if(query != ""):
+            print(query)
+            context = detail(query)
 
-    context = {"book_detail" : book_detail}
-    return render(request, 'BookDetect.html', context)
+        elif(query2 != ""):
+            print(query2)
+            context = detail(query2)
+    
+    # context = {"book_detail" : book_detail}
+     
+    return render(request, 'BookDetect.html', {"book_detail" : context})
+
+
+def detail(query):
+            book_detail = {}
+            if query != "":
+                url = "https://www.googleapis.com/books/v1/volumes?q={}&key=AIzaSyCVx4jHICEs3Dt8VYlxupTeibnYhBziBkU"
+                response = requests.get(url.format(query)).json()
+                # print(response['items'][0]['volumeInfo']['title'])
+                book_detail = {
+                        "book_name" : response['items'][0]['volumeInfo']['title'],
+                        "author_name" : response['items'][0]['volumeInfo']['authors'],
+                        "book_description" : response['items'][0]['volumeInfo']['description'],
+                        "book_img" : response['items'][0]['volumeInfo']['imageLinks']['smallThumbnail']
+                }
+            
+            return book_detail
 
